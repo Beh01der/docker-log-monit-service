1c90fdba04dbf43c2b6649a9fd3fe9f948df6f13
Andrey Chausenko <Andrey.Chausenko@team.telstra.com>
2016-04-05 18:21:15 +1000

    Updated dependencies

--- src/service.js
+++ src/service.js
@@ -3,43 +3,42 @@ var grok = require('node-grok');
 require('collections/shim-object');
 
 var empty = {};
-grok.loadDefault(function (patterns) {
-    console.log('Starting up docker log monitor...');
 
-    var SDC = require('statsd-client'),
-        sdc = new SDC({host: '172.17.42.1', port: 8125});
+var SDC = require('statsd-client'),
+    sdc = new SDC({host: '172.17.42.1', port: 8125});
 
-    var logPattern = patterns.createPattern('%{IP:client} \\[%{TIMESTAMP_ISO8601:timestamp}\\] "%{WORD:method}' +
-    ' %{URIHOST:site}%{URIPATHPARAM:url}" %{INT:code} %{INT:request} %{INT:response} - %{NUMBER:took}' +
-    ' \\[%{DATA:cache}\\] "%{DATA:mtag}" "%{DATA:agent}"');
+var patterns = grok.loadDefaultSync('grok-patterns');
+var logPattern = patterns.createPattern('%{IP:client} \\[%{TIMESTAMP_ISO8601:timestamp}\\] "%{WORD:method}' +
+' %{URIHOST:site}%{URIPATHPARAM:url}" %{INT:code} %{INT:request} %{INT:response} - %{NUMBER:took}' +
+' \\[%{DATA:cache}\\] "%{DATA:mtag}" "%{DATA:agent}"');
 
-    monitor(['router'], function (event) {
-        function logParsed(err, result) {
-            if (!err) {
-                Object.addEach(event, result || empty);
-                var code;
-                if (event.code && (code = parseInt(event.code))) {
-                    sdc.increment('router.hit');
-                    sdc.increment('router.hit.' + (Math.floor(code / 100) * 100));
-                }
-
-                if (event.url) {
-                    if (event.url.indexOf('api/note') != -1) {
-                        sdc.increment('api.note.hit');
-                        sdc.increment('api.hit');
-                    } else if (event.url.indexOf('api/policy') != -1) {
-                        sdc.increment('api.hit');
-                    }
-                }
+console.log('Starting up docker log monitor...');
+monitor(['router'], function (event) {
+    function logParsed(err, result) {
+        if (!err) {
+            Object.addEach(event, result || empty);
+            var code;
+            if (event.code && (code = parseInt(event.code))) {
+                sdc.increment('router.hit');
+                sdc.increment('router.hit.' + (Math.floor(code / 100) * 100));
             }
 
-            console.log(event);
+            if (event.url) {
+                if (event.url.indexOf('api/note') != -1) {
+                    sdc.increment('api.note.hit');
+                    sdc.increment('api.hit');
+                } else if (event.url.indexOf('api/policy') != -1) {
+                    sdc.increment('api.hit');
+                }
+            }
         }
 
-        if (event.log) {
-            logPattern.parse(event.log, logParsed);
-        } else {
-            console.log(event);
-        }
-    });
-}, ['grok-patterns']);
+        console.log(event);
+    }
+
+    if (event.log) {
+        logPattern.parse(event.log, logParsed);
+    } else {
+        console.log(event);
+    }
+});
